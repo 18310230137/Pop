2e3ed2c4ee82fbeebefc2af1147c2da61add8025
Ye_888 <137084265@qq.com>
2016-10-17 17:23:15 +0800

    Initial Commit

---
+++ Pop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pop
+//
+//  Created by BQHZ on 16/10/17.
+//  Copyright © 2016年 BQHZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
